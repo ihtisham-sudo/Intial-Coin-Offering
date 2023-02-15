ec9abaa50cd7e634912f74efd848dab9e8355de6
Ihtisham Ullah <ahsaanullah174@gmail.com>
2023-02-15 17:24:29 +0500

    updated

--- hardhat-tutorial/contracts/ICryptoDevs.sol
+++ hardhat-tutorial/contracts/ICryptoDevs.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.0;
 
 interface ICryptoDevs {
-    function tokenofOwnerByIndex (address owner, uint256 Index) external view returns (uint256 tokenId);
-    function balanceof (address owner) external view returns (uint256 balance);
+    function tokenOfOwnerByIndex (address owner, uint256 Index) external view returns (uint256 tokenId);
+    function balanceOf (address owner) external view returns (uint256 balance);
     
     
 }
