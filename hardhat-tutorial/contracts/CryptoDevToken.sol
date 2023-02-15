87caee81828bc99bbabaf67055303be4c674514c
Ihtisham Ullah <ahsaanullah174@gmail.com>
2023-02-15 15:33:27 +0500

    updating

--- hardhat-tutorial/contracts/CryptoDevToken.sol
+++ hardhat-tutorial/contracts/CryptoDevToken.sol
@@ -22,7 +22,34 @@ contract CryptoDevToken is ERC20, Ownable {
         require(totalSupply() + amountwithDecimals <= maxSupply, "Exceed the amount of max supply");
         _mint(msg.sender, amountwithDecimals);
     }
-    
+
+    function claim() public 
+    {
+        address sender  = msg.sender;
+        uint256 balance = CryptoDevsNFT.balanceof(sender);
+        require (balance > 0, "You don't own any crypto NFT");
+        uint256 amount = 0;
+        for (uint256 i = 0; i < balance ; i++)
+        {
+            uint256 tokenId = CryptoDevsNFT.tokenofOwnerByIndex(sender, i);
+            if (!tokenIdsClaimed[tokenId])
+            {
+                amount += 1;
+                tokenIdsClaimed[tokenId] = true;
+            }
+        }
+        require (amount > 0, "You have already claimed all the tokens");
+        _mint(msg.sender, amount * tokenperNFT);
+    }
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        require(amount > 0, "Nothing to withdraw, contract balance empty");
+        address _owner = owner();
+        (bool sent, ) = _owner.call{value: amount}("");
+        
+
+
+    }
 }
 
 
